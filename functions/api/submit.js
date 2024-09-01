f848ea7f496804d68578d4094da4ef67799c5b2e
Seiya-a <webeng.abu@gmail.com>
2024-09-01 21:32:50 +0900

    add form api key

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,9 +1,18 @@
 export async function onRequestPost(context) {
-  const input = await context.request.formData();
+  const MICROCMS_API_KEY = context.env.MICROCMS_API_KEY;
+  const SENDGRID_API_KEY = context.env.SENDGRID_API_KEY;
 
-  return new Response(JSON.stringify(input), {
+  const input = await context.request.formData();
+  const json = JSON.stringify(
+    [...input, MICROCMS_API_KEY, SENDGRID_API_KEY],
+    null,
+    2,
+  );
+
+  return new Response(json, {
     headers: {
       "Content-Type": "application/json",
     },
   });
 }
+
